dd58e37c4dbc0128fcceb542de4ed57b99c99b03
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-03-09 19:15:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 function secondHighest(arr) {
+
   if (arr.length < 2) return -Infinity;
 
   let first = -Infinity;
   let second = -Infinity;
 
   for (let i = 0; i < arr.length; i++) {
+
     if (arr[i] > first) {
       second = first;
       first = arr[i];
-    } 
+    }
+
     else if (arr[i] > second && arr[i] !== first) {
       second = arr[i];
     }
+
   }
 
   return second;
 }
-
-// input from prompt
-let n = Number(prompt());
-let arr = [];
-
-for (let i = 0; i < n; i++) {
-  arr.push(Number(prompt()));
-}
-
-alert(secondHighest(arr));
